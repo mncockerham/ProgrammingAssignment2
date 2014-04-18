839ef616ccb7eb83ac36d3782deb90cea9ca20b7
mncockerham <mncockerham@hotmail.com>
2014-04-17 17:53:20 -0700

    Commit New Functions
    Added my version of the functions to my version of the code base.

--- cachematrix.R
+++ cachematrix.R
@@ -3,36 +3,36 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   
-      #makes sure we have an empty variable
-      i <- NULL
-      ## Create a list to store values
-      set <- function(y) {
-               x <<- y    #Link to object
-               i <<- Null #Holder for the inverse
-      }
-      get <- function() x
-      setinvers <- function(solve) i <<- solve 
-      getinvers <- function() i
-      list(set = set, get = get,
-           setinvers = setinvers,
-           getinvers = getinvers)
+  #makes sure we have an empty variable
+  i <- NULL
+  ## Create a list to store values
+  set <- function(y) {
+    x <<- y    #Link to object
+    i <<- Null #Holder for the inverse
   }
+  get <- function() x
+  setinvers <- function(solve) i <<- solve 
+  getinvers <- function() i
+  list(set = set, get = get,
+       setinvers = setinvers,
+       getinvers = getinvers)
+}
 
 
 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    ## Attempts to get the casched value if it exits return it
-    i <- x$getinvers()
-    if(!is.null(i)) {
-       message("getting cached data")
-        return(i)
-    }
-    ## If it does not exits set calculate the value and save to I and return
-    data <- x$get()
-     i <- solve(data )
-    x$setinvers(i)
-    i
-   
+  ## Return a matrix that is the inverse of 'x'
+  ## Attempts to get the casched value if it exits return it
+  i <- x$getinvers()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  ## If it does not exits set calculate the value and save to I and return
+  data <- x$get()
+  i <- solve(data )
+  x$setinvers(i)
+  i
+  
 }
